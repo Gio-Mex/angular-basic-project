fe409bf3a2b50f59b80b6ccef5ba15e204a170bc
Giorgio Messore <giorgiomessore@MBP-di-Giorgio.homenet.telecomitalia.it>
2023-12-18 18:17:35 +0100

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatDividerModule } from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
+    ]),
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css'
+})
+export class CartComponent {
+items = this.cartService.getItems();
+
+checkoutForm = this.FormBuilder.group({
+  name: ['', Validators.required],
+  address: ['', Validators.required]
+})
+
+constructor(
+  private cartService: CartService,
+  private FormBuilder: FormBuilder
+  ){}
+
+onSubmit(): void {
+  this.items = this.cartService.clearCart();
+  console.warn('Your order has been submitted',
+  this.checkoutForm.value);
+  this.checkoutForm.reset();
+}
+}
+
